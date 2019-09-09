25f205d13c92d15d352171ef3bebe90fa1cd2a5c
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-09-09 09:27:25 -0500

    Update database Commit

--- WebApplicationMusicStore/Data/ApplicationDbContext.cs
+++ WebApplicationMusicStore/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebApplicationMusicStore.Models;
 
 namespace WebApplicationMusicStore.Data
 {
@@ -12,5 +13,6 @@ namespace WebApplicationMusicStore.Data
 			: base(options)
 		{
 		}
+		public DbSet<WebApplicationMusicStore.Models.Music> Music { get; set; }
 	}
 }

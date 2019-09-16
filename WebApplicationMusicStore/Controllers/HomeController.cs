147278f1d6d5034676fba912df830635bcab4e76
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-09-16 09:34:49 -0500

    home controller

--- WebApplicationMusicStore/Controllers/HomeController.cs
+++ WebApplicationMusicStore/Controllers/HomeController.cs
@@ -4,16 +4,25 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplicationMusicStore.Data;
 using WebApplicationMusicStore.Models;
 
 namespace WebApplicationMusicStore.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly ApplicationDbContext _context;
+
+		public HomeController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
 		public IActionResult Index()
 		{
-			return View();
+			return View(_context.Songs.Where(m => m.IsFeatured).ToList());
 		}
+
 
 		public IActionResult About()
 		{
